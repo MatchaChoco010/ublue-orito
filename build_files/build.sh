9c18fc4b13bfe4d4a92aacef316df2a31940afe4
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-19 20:39:53 +0900

    Add some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,20 +19,19 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 install -y gnome-tweaks niri xwayland-satellite zsh bat mako waybar
-dnf5 install -y wine winetricks --setopt=tsflags=nodocs || true
+dnf5 install -y gnome-tweaks niri xwayland-satellite zsh bat mako waybar wine winetricks
 
 # VSCode
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf5 check-update
-dnf5 install code # or code-insiders
+# rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+# dnf5 check-update
+# dnf5 install code # or code-insiders
 
 # 1Password
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-dnf5 check-update
-dnf5 install 1password
+# rpm --import https://downloads.1password.com/linux/keys/1password.asc
+# sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+# dnf5 check-update
+# dnf5 install 1password
 
 #### Example for enabling a System Unit File
 
