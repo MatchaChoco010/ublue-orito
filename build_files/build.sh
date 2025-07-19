766bc18a387d0772e8f6e05f9c8dbe63c48296b1
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-19 19:16:07 +0900

    Add some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,7 +19,7 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 install -y gnome-tweaks niri xwayland-satellite os-prober libratbag-ratbagd zsh eza bat mako waybar wine vulkan-headers vulkan-loader vulkan-tools 
+dnf5 install -y gnome-tweaks niri xwayland-satellite zsh bat mako waybar wine
 
 # VSCode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
