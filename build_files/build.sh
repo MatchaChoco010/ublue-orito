741f74b69f5631a2fc4ff4711064f6db56d43a95
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-19 22:42:51 +0900

    Add some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -26,14 +26,6 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y code # or code-insiders
 
-# 1Password
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
-
-ls -ld /opt
-
-dnf5 install -y 1password
-
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
