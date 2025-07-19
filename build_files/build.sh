d49013d36f6ebe913fe88f0ca6c20b2b3af01d3c
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-20 06:26:50 +0900

    Add some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-# dnf5 install -y tmux 
+# dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -19,12 +19,18 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 install -y gnome-tweaks niri xwayland-satellite swaybg fuzzel zsh bat btop mako waybar wine winetricks
+dnf5 install -y kernel-devel kernel-headers
+dnf5 install -y kmod-nvidia xorg-x11-drv-nvidia-cuda nvidia-driver-cuda
+dnf5 install -y gnome-tweaks niri xwayland-satellite swaybg fuzzel zsh bat btop mako blueman waybar wine winetricks xinput
+dnf5 remove -y alacritty
 
 # VSCode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y code # or code-insiders
+
+# XPPen
+dnf5 install -y https://download01.xp-pen.com/file/2025/02/XPPenLinux4.0.7-250117.rpm
 
 #### Example for enabling a System Unit File
 
