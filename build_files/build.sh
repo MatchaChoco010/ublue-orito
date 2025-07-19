d6828bd2de43c0282d74d7ccc0c55574b8426723
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-19 22:13:20 +0900

    Add some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,6 +29,9 @@ dnf5 install -y code # or code-insiders
 # 1Password
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
+
+ls -ld /opt/1Password
+
 dnf5 install -y 1password
 
 #### Example for enabling a System Unit File
