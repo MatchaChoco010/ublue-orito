5985a048a1d27933374e23d5855edadbdbb25c92
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-20 13:02:11 +0900

    Update to Bazzite

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,7 +23,7 @@ dnf5 install -y gnome-tweaks niri xwayland-satellite swaybg fuzzel zsh bat btop 
 
 # VSCode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y code # or code-insiders
 
 # XPPen
