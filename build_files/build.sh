a49da22f332b4c8ec7384f7b4b293cfc95e513f0
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-07-20 14:34:05 +0900

    Add some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,7 +19,7 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 install -y gnome-tweaks niri xwayland-satellite swaybg fuzzel zsh bat btop mako blueman waybar wine winetricks input-remapper firefox
+dnf5 install -y gnome-tweaks niri xwayland-satellite swaybg fuzzel zsh bat btop mako blueman waybar wine winetricks input-remapper firefox podman
 
 # VSCode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
