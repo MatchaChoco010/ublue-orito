25458feff0cbede5d0c409cb97244d54257d9a4c
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-08-29 10:16:48 +0900

    Add xdg-desktop-portal

--- build_files/build.sh
+++ build_files/build.sh
@@ -28,6 +28,8 @@ dnf5 install -y inotify-tools mako
 dnf5 install -y gnome-tweaks fuzzel input-remapper firefox foot
 dnf5 install -y adw-gtk3-theme papirus-icon-theme
 dnf5 install -y qt5ct qt6ct
+dnf5 install -y xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring
+dnf5 install -y nautilus
 
 dnf5 copr -y enable scottames/ghostty
 dnf5 install -y ghostty
