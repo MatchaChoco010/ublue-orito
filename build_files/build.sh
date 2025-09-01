3775e6b4539bc88124fde9138af2152167fc6014
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-09-01 10:34:29 +0900

    Add podman-compose

--- build_files/build.sh
+++ build_files/build.sh
@@ -30,6 +30,7 @@ dnf5 install -y adw-gtk3-theme papirus-icon-theme
 dnf5 install -y qt5ct qt6ct
 dnf5 install -y xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring
 dnf5 install -y nautilus
+dnf5 install -y podman-compose
 
 dnf5 copr -y enable scottames/ghostty
 dnf5 install -y ghostty
